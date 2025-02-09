616174e42c0922b600af3af697ecfbb3a4cc0155
Nikolay Zhechev <nikolayzhechev@yahoo.com>
2025-02-09 15:43:27 +0200

    Updated environment utility

--- Models/Environment_Creator.py
+++ Models/Environment_Creator.py
@@ -4,6 +4,8 @@ from SpacecraftOrbitalEnv import SpacecraftOrbitalEnv
 from SpacecraftInterplanetaryEnv import SpacecraftInterplanetaryEnv
 from SpacecraftDockingEnv_v2 import SpacecraftDockingEnv_v2
 from SimpleDockingEnv import SimpleDockingEnv
+from SpaceNavigationEnv import SpaceNavigationEnv
+from SpaceNavigationEnv_v2 import SpaceNavigationEnv_v2
 from enum import Enum
 
 # Define enmum for environments
@@ -14,25 +16,28 @@ Environments_enum = Enum(
         ('Orbital', 'orbital'),
         ('Interplanetary', 'interplanetary'),
         ('Docking_v2', 'docking_v2'),
-        ('Docking_simple', 'docking_simple')
+        ('Docking_simple', 'docking_simple'),
+        ('Navigation', 'navigation'),
+        ('Navigation_v2', 'navigation_v2')
     ]
 )
 
 def env_creator(environment_type, config=None):
     '''Instantiate a predefined environment. Environment type is selected from enum.'''
-    if environment_type == "docking":
-        return SpacecraftDockingEnv(config)
-
-    if environment_type == "orbital":
-        return SpacecraftOrbitalEnv(config)
-
-    if environment_type == "interplanetary":
-        return SpacecraftInterplanetaryEnv(config)
-
-    if environment_type == "docking_v2":
-        return SpacecraftDockingEnv_v2()
-
-    if environment_type == "docking_simple":
-        return SimpleDockingEnv()
-        
-    raise ValueError(f"Unknown environment type: {environment_type}")
+    match environment_type:
+        case "docking":
+            return SpacecraftDockingEnv(config)
+        case "docking_v2":
+            return SpacecraftDockingEnv_v2()
+        case "docking_simple":
+            return SimpleDockingEnv()
+        case "orbital":
+            return SpacecraftOrbitalEnv(config)
+        case "interplanetary":
+            return SpacecraftInterplanetaryEnv(config)
+        case "navigation":
+            return SpaceNavigationEnv()
+        case "navigation_v2":
+            return SpaceNavigationEnv_v2()
+        case _:
+            raise ValueError(f"Unknown environment type: {environment_type}")
